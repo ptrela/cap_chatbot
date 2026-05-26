87fbf7c5d02f611508d65a1445629999e28ac111
Paweł Trela <paweltrelapro@gmail.com>
2026-05-26 17:09:20 +0200

    fix host not permitted

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require_relative "jira_oauth"
 
 set :bind, "0.0.0.0"
 set :port, ENV.fetch("PORT", 3000)
-set :protection, except: [:host_authorization]
+set :protection, false
 
 ANTHROPIC_API = "https://api.anthropic.com/v1/messages"
 SLACK_API     = "https://slack.com/api"
